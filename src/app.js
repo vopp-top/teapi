6aadf9f2d2cca50943ccd87008081b907793b622
animekkk <animekgaming@gmail.com>
2022-02-19 14:59:25 +0100

    fixing

--- src/app.js
+++ src/app.js
@@ -46,7 +46,3 @@ app.use('/user/xayo', xayoRouter, globalLimiter);
 app.get('/', (request, response) => {
     response.status(200).json({ version: process.env.npm_package_version });
 });
-
-while(true) {
-    
-}
